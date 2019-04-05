89f650e7fcb93303f51860b26b246591f6b633b1
Sodbo Sharapov <sharapovsodbo@gmail.com>
2019-04-05 20:14:20 +0700

    Upd script for derived traits

--- plasma_derived_traits_2019.R
+++ plasma_derived_traits_2019.R
@@ -1,11 +1,5 @@
 # Copyright 2019 Sodbo Sharapov, Elgaeva Elizaveta, Lucija Klaric, Maja Pučić Baković, Yurii Aulchenko, Gordan Lauc
 
-# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
 plasma_derived_traits <- function(my_data){
 
 	my_data <- my_data[,grep(colnames(my_data), pattern='GP')]
@@ -174,6 +168,8 @@ plasma_derived_traits <- function(my_data){
 	my_data$PGP115 <- with(my_data, (0.5 * PGP12 + PGP13 + PGP14 + PGP15 + PGP16 + PGP17 + PGP19 + PGP20 + PGP21 + PGP22 + PGP23 + PGP24 + PGP25 + PGP26 + PGP27 + PGP28 + PGP29 + PGP30 + PGP31 + PGP32 + PGP33 + PGP34 + PGP35 + PGP36))
 
 	my_data$PGP116 <- with(my_data, (PGP3 + PGP4 + PGP5 + PGP6 + PGP8 + PGP9 + PGP10 + PGP11 + 0.5 * PGP12 + PGP13 + PGP14 + PGP15 + PGP16 + PGP17 + PGP19 + PGP20 + PGP21 + PGP22 + PGP23 + PGP24 + PGP25 + PGP26 + PGP27 + PGP28 + PGP29 + PGP30 + PGP31 + PGP32 + PGP33 + PGP34 + PGP35 + PGP36))
+
+	my_data$PGP117 <- with(my_data, (PGP3 + PGP4 + PGP5 + PGP6 + PGP8 + PGP9 + PGP10 + PGP11))
 
 	my_data <- my_data[, -c(1:36)]
 
